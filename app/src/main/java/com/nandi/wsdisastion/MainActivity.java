34996560c9da7507d4cfc705e2c11d0188be62e0
xingyufeifan <634846539@qq.com>
2017-07-14 10:14:36 +0800

    ....

--- app/src/main/java/com/nandi/wsdisastion/MainActivity.java
+++ app/src/main/java/com/nandi/wsdisastion/MainActivity.java
@@ -1,13 +1,18 @@
 package com.nandi.wsdisastion;
 
-import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -18,18 +23,14 @@ import com.esri.arcgisruntime.geometry.Point;
 import com.esri.arcgisruntime.geometry.SpatialReferences;
 import com.esri.arcgisruntime.layers.ArcGISMapImageLayer;
 import com.esri.arcgisruntime.layers.ArcGISSceneLayer;
-import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.ArcGISScene;
 import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
 import com.esri.arcgisruntime.mapping.Basemap;
-import com.esri.arcgisruntime.mapping.view.Callout;
 import com.esri.arcgisruntime.mapping.view.Camera;
-import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
 import com.esri.arcgisruntime.mapping.view.DefaultSceneViewOnTouchListener;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
 import com.esri.arcgisruntime.mapping.view.IdentifyGraphicsOverlayResult;
-import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.mapping.view.SceneView;
 import com.esri.arcgisruntime.symbology.PictureMarkerSymbol;
 import com.google.gson.Gson;
@@ -43,38 +44,47 @@ import java.util.concurrent.ExecutionException;
 
 import okhttp3.Call;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private SceneView mMapView;
     private GraphicsOverlay graphicsOverlay;
     private List<DisasterPoint> disasterPoints;
+    private ArcGISMapImageLayer imageLayer;
+    private ArcGISSceneLayer sceneLayer;
+    private ArcGISMapImageLayer shiLiangLayer;
+    private ArcGISMapImageLayer dianZiLayer;
+    private ArcGISMapImageLayer dengZXLayer;
+    private ArcGISMapImageLayer ssylLayer;
+    private ArcGISScene scene;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initData();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        graphicsOverlay = new GraphicsOverlay();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         // inflate MapView from layout
         mMapView = (SceneView) findViewById(R.id.sceneView);
         // create a MapImageLayer with dynamically generated map images
-        ArcGISMapImageLayer mapImageLayer = new ArcGISMapImageLayer(getResources().getString(R.string.world_elevation_service));
-        ArcGISSceneLayer sceneLayer=new ArcGISSceneLayer(getResources().getString(R.string.qinnxiesheying_url));
-//        ArcGISMapImageLayer imageLayer=new ArcGISMapImageLayer(getResources().getString(R.string.shiliangtu_url));
+        imageLayer = new ArcGISMapImageLayer(getResources().getString(R.string.world_elevation_service));
+        sceneLayer = new ArcGISSceneLayer(getResources().getString(R.string.qinnxiesheying_url));
+        shiLiangLayer = new ArcGISMapImageLayer(getResources().getString(R.string.shiliangtu_url));
+        dianZiLayer = new ArcGISMapImageLayer(getResources().getString(R.string.dianziditu_url));
+        dengZXLayer=new ArcGISMapImageLayer(getResources().getString(R.string.yuliang_url));
+        ssylLayer=new ArcGISMapImageLayer(getResources().getString(R.string.ssyl_url));
         // create an empty map instance
-        ArcGISScene scene=new ArcGISScene();
+        scene = new ArcGISScene();
         scene.setBasemap(Basemap.createImagery());
-        scene.getOperationalLayers().add(sceneLayer);
-        // add map image layer as operational layer
-        scene.getOperationalLayers().add(mapImageLayer);
-//        scene.getOperationalLayers().add(imageLayer);
-        // set the map to be displayed in this view
-        ArcGISTiledElevationSource elevationSource = new ArcGISTiledElevationSource(
-                getResources().getString(R.string.elevation_url));
-        scene.getBaseSurface().getElevationSources().add(elevationSource);
         mMapView.setScene(scene);
-        Camera camera = new Camera(30.07010,102.75326, 2000.0, 295.0, 20, 0.0);
-        mMapView.setViewpointCamera(camera);
-        graphicsOverlay = new GraphicsOverlay();
-        mMapView.getGraphicsOverlays().add(graphicsOverlay);
         mMapView.setOnTouchListener(new DefaultSceneViewOnTouchListener(mMapView) {
             @Override
             public boolean onSingleTapConfirmed(MotionEvent e) {
@@ -88,10 +98,10 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         try {
                             IdentifyGraphicsOverlayResult identifyGraphicsOverlayResult = identifyGraphic.get();
-                            if (identifyGraphicsOverlayResult.getGraphics().size()>0){
+                            if (identifyGraphicsOverlayResult.getGraphics().size() > 0) {
 
-                            int zIndex = identifyGraphicsOverlayResult.getGraphics().get(0).getZIndex();
-                            showInfo(zIndex);
+                                int zIndex = identifyGraphicsOverlayResult.getGraphics().get(0).getZIndex();
+                                showInfo(zIndex);
                             }
                         } catch (InterruptedException | ExecutionException e1) {
                             e1.printStackTrace();
@@ -106,38 +116,39 @@ public class MainActivity extends AppCompatActivity {
 
     private void showInfo(int zIndex) {
         OkHttpUtils.get().url(getResources().getString(R.string.get_disaster_info))
-                .addParams("id",zIndex+"")
+                .addParams("id", zIndex + "")
                 .build()
                 .execute(new StringCallback() {
                     @Override
                     public void onError(Call call, Exception e, int id) {
-                        Toast.makeText(getApplicationContext(),"网络连接失败！",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getApplicationContext(), "网络连接失败！", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
                     public void onResponse(String response, int id) {
-                        Gson gson=new Gson();
-                        Type type = new TypeToken<List<DisasterInfo>>() {}.getType();
-                        List<DisasterInfo> disasterInfos=gson.fromJson(response,type);
-                        DisasterInfo disasterInfo=disasterInfos.get(0);
-                        String info="名称："+disasterInfo.getDis_name()+"\n"
-                                +"地点："+disasterInfo.getDis_location()+"\n"
-                                +"经纬度："+disasterInfo.getDis_lon()+","+disasterInfo.getDis_lat()+"\n"
-                                +"灾害因素："+disasterInfo.getDis_cause()+"\n"
-                                +"受灾面积："+disasterInfo.getDis_area()+"\n"
-                                +"受灾体积："+disasterInfo.getDis_volume()+"\n"
-                                +"威胁户数："+disasterInfo.getImperil_families()+"\n"
-                                +"威胁人数："+disasterInfo.getImperil_man()+"\n"
-                                +"威胁房屋："+disasterInfo.getImperil_house()+"\n"
-                                +"威胁房屋面积："+disasterInfo.getImperil_area()+"\n"
-                                +"影响对象："+disasterInfo.getMain_object()+"\n"
-                                +"威胁财产："+disasterInfo.getImperil_money()+"\n"
-                                +"灾害等级："+disasterInfo.getImperil_level()+"\n"
-                                +"是否涉水："+(disasterInfo.getDis_sfss()==1?"是":"否")+"\n"
-                                +"告警号码:"+disasterInfo.getWarn_mobile()+"\n"
-                                +"入库时间:"+disasterInfo.getCome_time()+"\n";
-                        View view= LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_disaster_info,null);
-                        TextView tvInfo= (TextView) view.findViewById(R.id.dialog_text);
+                        Gson gson = new Gson();
+                        Type type = new TypeToken<List<DisasterInfo>>() {
+                        }.getType();
+                        List<DisasterInfo> disasterInfos = gson.fromJson(response, type);
+                        DisasterInfo disasterInfo = disasterInfos.get(0);
+                        String info = "名称：" + disasterInfo.getDis_name() + "\n"
+                                + "地点：" + disasterInfo.getDis_location() + "\n"
+                                + "经纬度：" + disasterInfo.getDis_lon() + "," + disasterInfo.getDis_lat() + "\n"
+                                + "灾害因素：" + disasterInfo.getDis_cause() + "\n"
+                                + "受灾面积：" + disasterInfo.getDis_area() + "\n"
+                                + "受灾体积：" + disasterInfo.getDis_volume() + "\n"
+                                + "威胁户数：" + disasterInfo.getImperil_families() + "\n"
+                                + "威胁人数：" + disasterInfo.getImperil_man() + "\n"
+                                + "威胁房屋：" + disasterInfo.getImperil_house() + "\n"
+                                + "威胁房屋面积：" + disasterInfo.getImperil_area() + "\n"
+                                + "影响对象：" + disasterInfo.getMain_object() + "\n"
+                                + "威胁财产：" + disasterInfo.getImperil_money() + "\n"
+                                + "灾害等级：" + disasterInfo.getImperil_level() + "\n"
+                                + "是否涉水：" + (disasterInfo.getDis_sfss() == 1 ? "是" : "否") + "\n"
+                                + "告警号码:" + disasterInfo.getWarn_mobile() + "\n"
+                                + "入库时间:" + disasterInfo.getCome_time() + "\n";
+                        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_disaster_info, null);
+                        TextView tvInfo = (TextView) view.findViewById(R.id.dialog_text);
                         tvInfo.setText(info);
                         new AlertDialog.Builder(MainActivity.this)
                                 .setView(view)
@@ -148,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void setOverlay() {
-        BitmapDrawable pinStarBlueDrawable = (BitmapDrawable) ContextCompat.getDrawable(this, R.mipmap.sign);
+        BitmapDrawable pinStarBlueDrawable = (BitmapDrawable) ContextCompat.getDrawable(this, R.mipmap.ic_launcher);
         final PictureMarkerSymbol pinStarBlueSymbol = new PictureMarkerSymbol(pinStarBlueDrawable);
         //Optionally set the size, if not set the image will be auto sized based on its size in pixels,
         //its appearance would then differ across devices with different resolutions.
@@ -163,8 +174,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 for (DisasterPoint disasterPoint : disasterPoints) {
-                    Point point=new Point(Double.valueOf(disasterPoint.getDis_lon()),Double.valueOf(disasterPoint.getDis_lat()),SpatialReferences.getWgs84());
-                    Graphic graphic=new Graphic(point,pinStarBlueSymbol);
+                    Point point = new Point(Double.valueOf(disasterPoint.getDis_lon()), Double.valueOf(disasterPoint.getDis_lat()), SpatialReferences.getWgs84());
+                    Graphic graphic = new Graphic(point, pinStarBlueSymbol);
                     graphic.setZIndex(disasterPoint.getId());
                     graphicsOverlay.getGraphics().add(graphic);
                 }
@@ -184,12 +195,87 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response, int id) {
                         Gson gson = new Gson();
-                        Type type = new TypeToken<List<DisasterPoint>>() {}.getType();
+                        Type type = new TypeToken<List<DisasterPoint>>() {
+                        }.getType();
                         disasterPoints = gson.fromJson(response, type);
-                        Log.d("WSD","集合大小："+disasterPoints.size()+"\n数据:"+disasterPoints);
+                        Log.d("WSD", "集合大小：" + disasterPoints.size() + "\n数据:" + disasterPoints);
                         setOverlay();
                     }
                 });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_slt) {
+            toolbar.setTitle("矢量图");
+            mMapView.getGraphicsOverlays().clear();
+            scene.getOperationalLayers().clear();
+            scene.getBaseSurface().getElevationSources().clear();
+            scene.getOperationalLayers().add(shiLiangLayer);
+            Camera camera = new Camera(28.969167, 106.950399, 50000.0, 295.0, 20, 0.0);
+            mMapView.setViewpointCamera(camera);
+        } else if (id == R.id.nav_dzdt) {
+            toolbar.setTitle("电子地图");
+            mMapView.getGraphicsOverlays().clear();
+            scene.getOperationalLayers().clear();
+            scene.getBaseSurface().getElevationSources().clear();
+            scene.getOperationalLayers().add(dianZiLayer);
+            Camera camera = new Camera(28.969167, 106.950399, 50000.0, 295.0, 20, 0.0);
+            mMapView.setViewpointCamera(camera);
+        } else if (id == R.id.nav_yxt) {
+            toolbar.setTitle("影像图");
+            mMapView.getGraphicsOverlays().add(graphicsOverlay);
+            initData();
+            scene.getOperationalLayers().clear();
+            ArcGISTiledElevationSource elevationSource = new ArcGISTiledElevationSource(
+                    getResources().getString(R.string.elevation_url));
+            scene.getBaseSurface().getElevationSources().add(elevationSource);
+            scene.getOperationalLayers().add(imageLayer);
+            Camera camera = new Camera(28.969167, 106.950399, 50000.0, 295.0, 20, 0.0);
+            mMapView.setViewpointCamera(camera);
+        } else if (id == R.id.nav_qxsy) {
+            toolbar.setTitle("倾斜摄影");
+            scene.getOperationalLayers().clear();
+            scene.getBaseSurface().getElevationSources().clear();
+            scene.getOperationalLayers().add(sceneLayer);
+            Camera camera = new Camera(30.07010,102.75326, 1289.0, 295.0, 20, 0.0);
+            mMapView.setViewpointCamera(camera);
+        } else if(id==R.id.nav_dzx){
+            toolbar.setTitle("雨量等值线");
+            scene.getOperationalLayers().clear();
+            mMapView.getGraphicsOverlays().clear();
+            scene.getBaseSurface().getElevationSources().clear();
+            scene.getOperationalLayers().add(dengZXLayer);
+            Camera camera = new Camera(28.969167, 106.950399, 50000.0, 295.0, 20, 0.0);
+            mMapView.setViewpointCamera(camera);
+        }else if (id==R.id.nav_ssyl){
+            toolbar.setTitle("实时雨量");
+            scene.getOperationalLayers().clear();
+            mMapView.getGraphicsOverlays().clear();
+            scene.getBaseSurface().getElevationSources().clear();
+            scene.getOperationalLayers().add(shiLiangLayer);
+            scene.getOperationalLayers().add(ssylLayer);
+            Camera camera = new Camera(28.969167, 106.950399, 50000.0, 295.0, 20, 0.0);
+            mMapView.setViewpointCamera(camera);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
